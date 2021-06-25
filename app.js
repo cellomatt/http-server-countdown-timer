3a3aa00c20e7ba73679eea278fbb705951198eaf
Matt Kufchak <matt.kufchak@gmail.com>
2021-06-25 12:37:32 -0500

    successfully posting end time of timer to server

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 const fs = require('fs');
 
-let endtime;
+let endtime = null;
+
 
 const server = http.createServer((req, res) => {
   let reqBody = '';
@@ -28,18 +29,64 @@ const server = http.createServer((req, res) => {
       const home = fs.readFileSync("index.html")
       res.setStatusCode = 200;
       res.setHeader("Content-Type", "text/html");
-      res.write(home)
-      return res.end();
+      return res.end(home);
+    }
+
+    if (req.url === "/scripts.js") {
+      var file =  __dirname + "/scripts.js";
+      var stat = fs.statSync(file);
+      res.writeHead(200, {
+        'Content-Type': 'text/javascript',
+        'Content-Length': stat.size
+      });
+      var readStream = fs.createReadStream(file);
+      readStream.pipe(res);
+    }
+
+    if (req.url === "/app.js") {
+      var file =  __dirname + "/app.js";
+      var stat = fs.statSync(file);
+      res.writeHead(200, {
+        'Content-Type': 'text/javascript',
+        'Content-Length': stat.size
+      });
+      var readStream = fs.createReadStream(file);
+      readStream.pipe(res);
     }
 
     if (req.method === "GET" && req.url === "/timer") {
-      const home = fs.readFileSync("countdown.html")
+      const timer = fs.readFileSync("countdown.html")
       res.setStatusCode = 200;
       res.setHeader("Content-Type", "text/html");
-      res.write(home)
+      return res.end(timer);
+    }
+
+    if (req.method === "POST" && req.url === "/timer") {
+      endtime = new Date()
+      console.log(endtime)
+      let currentHours = endtime.getHours()
+      let currentMinutes = endtime.getMinutes()
+      let currentSeconds = endtime.getSeconds()
+      console.log(currentHours, currentMinutes, currentSeconds)
+      currentHours += Number(req.body.hours)
+      currentMinutes += Number(req.body.minutes)
+      currentSeconds += Number(req.body.seconds)
+      if (currentSeconds > 59) {
+        currentMinutes += 1
+        currentSeconds -= 60
+      }
+      if (currentMinutes > 59) {
+        currentHours += 1
+        currentMinutes -= 60
+      }
+      endtime.setHours(currentHours, currentMinutes, currentSeconds)
+      res.writeHead(302, {
+        'Content-Type': 'text/html',
+        'Location': '/timer'
+      });
       return res.end();
     }
-    
+
 
   });
 });
@@ -47,3 +94,5 @@ const server = http.createServer((req, res) => {
 const port = 5000;
 
 server.listen(port, () => console.log('Server is listening on port', port));
+
+module.exports = { endtime }
---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("loaded")
+
+  const timer = document.getElementById("timer")
+  timer.addEventListener("click", () => {
+    console.log("clicked the timer")
+  })
+
+  const updateTimer = (endtime) => {
+
+  }
+})
